27c17e5c56cb04cf9f3ea09a359efe55f33e2258
yousef <m.yousef.r84@gmail.com>
2025-05-10 17:28:18 +0330

    dibug

--- Areza_Yabi/settings.py
+++ Areza_Yabi/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-to8qfil2(pa=01^_gm=!2ft#$))agha0ado^_kt_m#rn#4=d++
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -100,25 +100,22 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'innoneti_Areze_yab',
-#         'USER': 'innoneti_admin',
-#         'PASSWORD': 'm.yousef.r84',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 REST_FRAMEWORK = {
